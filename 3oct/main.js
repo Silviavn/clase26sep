85a2df64400052d87a32fa3e0f4102b0a03577ac
silvia <silviavergara.14sa@gmail.com>
2022-10-04 11:23:00 -0300

    clase3sep

---
+++ 3oct/main.js
@@ -0,0 +1,22 @@
+const saludo = () => {
+
+    console.log("Hola buenos dias")   //No retorna 
+
+}
+
+const getSalute = (name) => {
+    return " muy buenos dias " + name
+}
+
+saludo()
+console.log(getSalute('Que tengas un lindo dia )) 
+console.log(getSalute('marina'))
+console.log("que tengas un buen dia "
+
+const getFullName = ( name, apellido1, apellido2) => {
+     return  name + ""+ name +""+ apellido1 +""+ apellido2
+
+}
+const getFullName ("silvia", "vergara", "nuñez");
+console.log(getFullName, "tu nombre tiene 20caracteres",getFullName.length "jaja")
+
